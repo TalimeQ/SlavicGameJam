9904568c7be07fa9d6c2faa44551a3ee9c6d1880
Krzychor <37249796+Krzychor@users.noreply.github.com>
2019-07-19 22:04:28 +0200

    coop movement + player hp

--- Assets/Scripts/MovementSecond.cs
+++ Assets/Scripts/MovementSecond.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+//Movement for second player
+public class MovementSecond : MonoBehaviour
 {
     [SerializeField] string HorizontalInputAxis;
     [SerializeField] string VerticalInputAxis;
+    [SerializeField] string MouseAxis;
+    [SerializeField] float RotateSpeed = 100; //only for rotating with arrows
     [SerializeField] float speed = 20;
     Rigidbody2D rigid;
 
@@ -14,14 +17,13 @@ public class Movement : MonoBehaviour
     {
         rigid = GetComponent<Rigidbody2D>();
     }
-
-
+    
     void Update()
     {
-        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        diff.Normalize(); 
-        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(0, 0, -RotateSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(0, 0, RotateSpeed * Time.deltaTime);
 
         Vector2 direction = new Vector2(Input.GetAxis(HorizontalInputAxis), Input.GetAxis(VerticalInputAxis));
         rigid.MovePosition(new Vector2(transform.position.x, transform.position.y) + direction * speed * Time.deltaTime);
