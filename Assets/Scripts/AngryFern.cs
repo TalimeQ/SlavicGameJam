00f7c844a9be048e3757a1091036ce1dd2f57fc2
Talimeq <boren1996@wp.pl>
2019-07-19 17:52:21 +0200

    Small Fern Changes

--- Assets/Scripts/AngryFern.cs
+++ Assets/Scripts/AngryFern.cs
@@ -21,7 +21,7 @@ public class AngryFern : MonoBehaviour
     {
         isBeingDamaged = true;
         health -= Damage * Time.deltaTime;
-        transform.localScale -= growthRate * transform.localScale * Time.deltaTime;
+        transform.localScale -= Damage * transform.localScale * Time.deltaTime;
         if (health <= 0)
         {
             health = 0;
