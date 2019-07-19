fde02b201ba3f74fe9b79f8fa35bb32ee6a8aec1
Talimeq <boren1996@wp.pl>
2019-07-19 23:06:17 +0200

    Spawn point class

---
+++ Assets/SpawnPoint.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+
+    [SerializeField] private GameObject spawnedFern = null;
+    FernIgnore ignoredAxis;
+    float ignoredSign;
+
+    private SpawnerController spawningController; 
+
+    public void Init(SpawnerController currentSpawner)
+    {
+        spawningController = currentSpawner;
+        SpawnFern();
+    }
+
+    public void OnBoundsExtended()
+    {
+        SpawnFern();
+    }
+
+    private void SpawnFern()
+    {
+        if(spawnedFern != null)
+        {
+            GameObject spawnedFern = Instantiate(gameObject, this.transform);
+            spawnedFern.GetComponent<AngryFern>()?.Init()
+        }
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+}
