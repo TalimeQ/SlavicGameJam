f1528c49c518840899a0ac58f6d28983e08280b1
tangosh <tangosh42@gmail.com>
2019-07-19 21:30:49 +0200

    random spawn points

--- Assets/SpawnerController.cs
+++ Assets/SpawnerController.cs
@@ -3,31 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnerController : MonoBehaviour
-{
-public GameObject plantObject;
+    {
+    public GameObject plantObject;
+    public GameObject SpawnPointParent;
+    private List<Vector3> spawnPoints = new List<Vector3>();
+    [SerializeField]
+    private float timeBetweenSpawns;
+
+    int nextSpawnPointID;
 
 
+    void Start()
+    {
+        foreach (Transform child in SpawnPointParent.transform)
+        {
+            spawnPoints.Add(child.position);
+        } 
+    }
 
-[SerializeField]
-private float timeBetweenSpawns;
-
-    IEnumerator SpawnPlant()
+    IEnumerator SpawnPlant(Vector3 spawnTransform)
     {
         yield return new WaitForSeconds(timeBetweenSpawns);
-        Instantiate(plantObject, transform.position, Quaternion.identity);
-        
-        
-
+        Instantiate(plantObject, spawnTransform, Quaternion.identity);
     }
 
     public void BeginSpawningPlants()
     {
-        while (true)
-        StartCoroutine(SpawnPlant());
+            nextSpawnPointID = Random.Range(0, spawnPoints.Count);
+            StartCoroutine(SpawnPlant(spawnPoints[nextSpawnPointID]));
     }
 
-    public void StopSpawningPlants()
+    private void GenerateRandomSpawnID()
     {
-        StopCoroutine(SpawnPlant());
+        
     }
 }
