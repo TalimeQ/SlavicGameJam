ff6d8451bde6ea748ee2a03c419583fb58e24e0a
tangosh <tangosh42@gmail.com>
2019-07-20 16:54:58 +0200

    Dodanie dźwięków chodzenia

---
+++ Assets/PlayerSoundController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSoundController : MonoBehaviour
+{ 
+    [SerializeField] private string HorizontalAxisString;
+    [SerializeField] private string VerticalAxisString;
+    private AudioSource audioSource;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton(HorizontalAxisString) || Input.GetButton(VerticalAxisString))
+        {
+            if (!audioSource.isPlaying)
+            {
+            audioSource.Play();
+            }
+        }
+    }
+}
