f09e64d19b170c86b8d1bae52b9aa0cd5c896007
tangosh <tangosh42@gmail.com>
2019-07-20 12:15:19 +0200

    Podmianka assetów, zmiana colliderów na playerach

--- Assets/scripts/movement.cs
+++ Assets/scripts/movement.cs
@@ -28,9 +28,11 @@ public class Movement : MonoBehaviour
     {
         rigid.MovePosition(new Vector2(transform.position.x, transform.position.y) + direction * speed * Time.fixedDeltaTime);
 
+
         Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         diff.Normalize();
         float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+        
     }
 }
